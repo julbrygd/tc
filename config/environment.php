f3f626de8d00861496a9a0707328fdb2eb59218b
root <root@debian>
2017-09-22 17:34:56 +0200

    first commit

---
+++ config/environment.php
@@ -0,0 +1,9 @@
+<?php
+
+/*----------------------------------------------------*/
+// Define environment type
+/*----------------------------------------------------*/
+return [
+    'local' => 'http://localhost:18080',
+    'production' => 'INSERT-HOSTNAME'
+];
