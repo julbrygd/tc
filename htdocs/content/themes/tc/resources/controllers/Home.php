1c7a031524269e601666ed3fd8131fbd1295a9b3
julbrygd <stephan@conrad.pics>
2017-09-24 18:17:45 +0200

    Startseite

---
+++ htdocs/content/themes/tc/resources/controllers/Home.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Theme\Controllers;
+
+use Themosis\Route\BaseController;
+use Theme\Models\Posts;
+
+/**
+ * Description of Home
+ *
+ * @author stephan
+ */
+class Home extends BaseController {
+
+    private static $START_PAGE_KEY = 'page_on_front';
+
+    public function home() {
+        $startpage = get_option(self::$START_PAGE_KEY);
+        $posttype = get_option('show_on_front');
+        $post = new Posts(new \WP_Query());
+        return view("welcome", array("post" => $post->find([
+                'p' => $startpage,
+                'post_type' => $posttype
+            ])->first()->get()));
+    }
+
+}
