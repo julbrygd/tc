4aa6dfd298d0c4b3e57c6c8523e92a69f20d86dd
julbrygd <stephan@conrad.pics>
2017-09-24 17:17:06 +0200

    Installation auf Macbook

--- config/environment.php
+++ config/environment.php
@@ -4,7 +4,7 @@
 // Define environment type
 /*----------------------------------------------------*/
 return [
-    'dev' => 'debian',
+    'dev' => ['debian','stephan-mac', 'stephan-mac\.lan\.home\.conrad\.pics'],
     'int' => 'kosmos\.metanet\.ch',
     'production' => 'INSERT-HOSTNAME'
 ];
